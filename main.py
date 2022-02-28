9ea611a18651fa8045fccebc20ba6fd309e6b30e
Hongyi Zhang <zhanghongyi@bupt.edu.cn>
2022-03-01 01:12:47 +0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(host='82.156.172.44', port=5000, debug=True)
+
 
 @app.route("/")
 def hello_world():
