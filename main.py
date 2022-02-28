57e2a8f90fe9586614fdcdc3d93ea1ea544e9147
Hongyi Zhang <zhanghongyi@bupt.edu.cn>
2022-03-01 01:24:32 +0800

    update

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='82.156.172.44', port=5000, debug=True)
-
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True)
